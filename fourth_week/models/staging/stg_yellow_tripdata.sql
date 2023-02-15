ccd704cc304a8aadadca47148f83a7e8f73ca76b
Sergey Mangilev <ssmangilev@gmail.com>
2023-02-15 05:42:23 +0100

    Cast for the vendorID was added

--- fourth_week/models/staging/stg_yellow_tripdata.sql
+++ fourth_week/models/staging/stg_yellow_tripdata.sql
@@ -3,7 +3,7 @@
 with tripdata as 
 (
   select *,
-    row_number() over(partition by vendorid, tpep_pickup_datetime) as rn
+    row_number() over(partition by CAST(vendorid as integer), tpep_pickup_datetime) as rn
   from {{ source('staging','yellow_tripdata') }}
   where vendorid is not null 
 )
